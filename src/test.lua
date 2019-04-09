4bb4993c68bc5f3a333189e954648c50b9828208
reasonmix <248587660@qq.com>
2019-04-09 10:58:14 +0800

    ff

--- src/test.lua
+++ src/test.lua
@@ -1,9 +1,11 @@
 local cjson = require('cjson')
 
-local function test(a,b,c,d,e,f,g)
-	local ret = a + b + c + d + e + f + g
+local function test(a,b,c,d,e,...)
+	local ret = a + b + c + d + e
 	print('ret is ',ret)
 	return ret
 end
 
-test(10,20,30,40,50,60,70)
+local ret = test(10,20,30,40,50,60,70)
+print('ret is ',ret)
+--cjson.decode()
